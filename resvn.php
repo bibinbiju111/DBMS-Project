aad46a73892c395f6c2f21624cc428ffd125bc0e
Anu Mathew <79563511+anumathew-25@users.noreply.github.com>
2022-07-29 00:20:53 +0530

    Update resvn.php

--- resvn.php
+++ resvn.php
@@ -1,10 +1,36 @@
-<html>
-<body style=" background-image: url(pnglogin.jpg);
-    height: 100%; 
-    background-position: center;
-    background-repeat: no-repeat;
-    background-size: cover;" >
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>RAILWAY SYSTEM</title>
+    <link rel="stylesheet" href="style.css">
+    <link href='https://cdn.jsdelivr.net/npm/boxicons@2.0.5/css/boxicons.min.css' rel='stylesheet'>
+</head>
+<body>
+    <header class="header">
+        <a href="index.htm">
+            <img src="images/logo.png" alt="Logo" class="logo">
+        </a>
+        <nav class="menu">
+            <ul>
+                <li>
+                    <a href="http://localhost/railway/new_user_form.html">New User</a>
+                </li>
+                <li>
+                    <a href="http://localhost/railway/enquiry.php" >Enquiry and Ticket Booking </a>
+                </li>
+                <li>
+                    <a href="http://localhost/railway/user_login.htm">Ticket History OR Cancellation</a>
+                </li>
+                <li>
+                    <a href="http://localhost/railway/admin_login.php">Admin Login</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+<div class="content-area">
 
 <form action="new_png.php" method="post">
 
@@ -62,6 +88,16 @@ $conn->close();
 
 ?>
 
-<br><br><input type="submit" value="Book">
+<br><br><input type="submit" value="Book" class="btn"></form>
+</div>
+<footer class="footer">
+        <p>All rights reserved</p>
+        <div class="social-media">
+            <a href="https://www.facebook.com/IRCTCofficial/" target="_blank" class="social-icon"><i class='bx bxl-facebook'></i></a>
+            <a href="https://instagram.com/irctc.official" target="_blank" class="social-icon"><i class='bx bxl-instagram' ></i></a>
+            <a href="https://twitter.com/IRCTCofficial" target="_blank" class="social-icon"><i class='bx bxl-twitter' ></i></a>
+        </div>
+
+    </footer>
 </body>
 </html>
