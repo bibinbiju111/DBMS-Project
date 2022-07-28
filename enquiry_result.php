440e1e6c5c0819c227ceb62573cb844fc30938a0
Anu Mathew <79563511+anumathew-25@users.noreply.github.com>
2022-07-29 00:11:14 +0530

    Update enquiry_result.php

--- enquiry_result.php
+++ enquiry_result.php
@@ -1,9 +1,36 @@
-<html>
-<body style=" background-image: url(pnglogin.jpg);
-    height: 100%; 
-    background-position: center;
-    background-repeat: no-repeat;
-    background-size: cover;" >
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>RAILWAY SYSTEM</title>
+    <link rel="stylesheet" href="style.css">
+    <link href='https://cdn.jsdelivr.net/npm/boxicons@2.0.5/css/boxicons.min.css' rel='stylesheet'>
+</head>
+<body>
+    <header class="header">
+        <a href="index.htm">
+            <img src="images/logo.png" alt="Logo" class="logo">
+        </a>
+        <nav class="menu">
+            <ul>
+                <li>
+                    <a href="new_user_form.html">New User</a>
+                </li>
+                <li>
+                    <a href="enquiry.php" >Enquiry and Ticket Booking </a>
+                </li>
+                <li>
+                    <a href="user_login.html">Ticket History OR Cancellation</a>
+                </li>
+                <li>
+                    <a href="admin_login.php">Admin Login</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+    <div class="enquiry-result-wrap align-center">
 
 <?php 
 
@@ -31,7 +58,7 @@ echo "</table>";
 //$rowcount=mysqli_num_rows($query);
 if(mysqli_num_rows($query) == 0)
 {
- echo "No such train <br> ";
+ echo "No such train <br><br> ";
 
 }
 ?>
@@ -43,16 +70,26 @@ Password: <input type="password" name="password" required><br><br>
 Enter Train No: <input type="text" name="tno" required><br><br>
 Enter Class: <input type="text" name="class" required><br><br>
 No. of Seats: <input type="text" name="nos" required><br><br>
-<input type="submit" value="Proceed with Booking"><br><br>
-</form>
+<input type="submit" value="Proceed with Booking" class="btn"><br><br>
+</form><br><br>
 
 <?php
 
-echo " <a href=\"http://localhost/railway/enquiry.php\">More Enquiry</a> <br>";
+echo " <a class=\"btn-link\" href=\"http://localhost/railway/enquiry.php\">More Enquiry</a> <br>";
 
 $conn->close(); 
 ?>
+</div>
 
-<br><a href="http://localhost/railway/index.htm">Go to Home Page!!!</a>
+<footer class="footer">
+    <p>All rights reserved</p>
+    <div class="social-media">
+        <a href="https://www.facebook.com/IRCTCofficial/" target="_blank" class="social-icon"><i class='bx bxl-facebook'></i></a>
+        <a href="https://instagram.com/irctc.official" target="_blank" class="social-icon"><i class='bx bxl-instagram' ></i></a>
+        <a href="https://twitter.com/IRCTCofficial" target="_blank" class="social-icon"><i class='bx bxl-twitter' ></i></a>
+    </div>
+
+</footer>
 </body>
 </html>
+
