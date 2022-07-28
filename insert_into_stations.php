5bdf77449799d2dc93b8fa111b045a9ece556f27
Anu Mathew <79563511+anumathew-25@users.noreply.github.com>
2022-07-29 00:14:44 +0530

    Update insert_into_stations.php

--- insert_into_stations.php
+++ insert_into_stations.php
@@ -1,11 +1,37 @@
-<html>
-<body style=" background-image: url(adminlogin.jpeg);
-    height: 100%; 
-    background-position: center;
-    background-repeat: no-repeat;
-    background-size: cover;">
 
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>RAILWAY SYSTEM</title>
+    <link rel="stylesheet" href="style.css">
+    <link href='https://cdn.jsdelivr.net/npm/boxicons@2.0.5/css/boxicons.min.css' rel='stylesheet'>
+</head>
+<body>
+    <header class="header">
+        <a href="index.htm">
+            <img src="images/logo.png" alt="Logo" class="logo">
+        </a>
+        <nav class="menu">
+            <ul>
+                <li>
+                    <a href="http://localhost/railway/new_user_form.html">New User</a>
+                </li>
+                <li>
+                    <a href="http://localhost/railway/enquiry.php" >Enquiry and Ticket Booking </a>
+                </li>
+                <li>
+                    <a href="http://localhost/railway/user_login.htm">Ticket History OR Cancellation</a>
+                </li>
+                <li>
+                    <a href="http://localhost/railway/admin_login.php">Admin Login</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+<div class="content-area">
 <?php
 
 require "db.php";
@@ -34,5 +60,15 @@ Add Station : <input type="text" name="sname" placeholder=" New Station" require
 <?php
 echo "<br><br> <a href=\"http://localhost/railway/admin_login.php\">Go Back to Admin Menu!!!</a> ";
 ?>
+</div>
+<footer class="footer">
+        <p>All rights reserved</p>
+        <div class="social-media">
+            <a href="https://www.facebook.com/IRCTCofficial/" target="_blank" class="social-icon"><i class='bx bxl-facebook'></i></a>
+            <a href="https://instagram.com/irctc.official" target="_blank" class="social-icon"><i class='bx bxl-instagram' ></i></a>
+            <a href="https://twitter.com/IRCTCofficial" target="_blank" class="social-icon"><i class='bx bxl-twitter' ></i></a>
+        </div>
+
+    </footer>
 </body>
 </html>
