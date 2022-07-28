9010b89142eba6bfdb2153c3ea5836f64d4e52a5
Anu Mathew <79563511+anumathew-25@users.noreply.github.com>
2022-07-28 23:57:12 +0530

    Update admin_login.php

--- admin_login.php
+++ admin_login.php
@@ -1,20 +1,54 @@
-<html>
-<body style=" background-image: url(adminlogin.jpeg);
-    height: 100%; 
-    background-position: center;
-    background-repeat: no-repeat;
-    background-size: cover;">
-<div align="center">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>RAILWAY SYSTEM</title>
+    <link rel="stylesheet" href="style.css">
+    <link href='https://cdn.jsdelivr.net/npm/boxicons@2.0.5/css/boxicons.min.css' rel='stylesheet'>
+</head>
+<body>
+    <header class="header">
+        <a href="index.htm">
+            <img src="images/logo.png" alt="Logo" class="logo">
+        </a>
+        <nav class="menu">
+            <ul>
+                <li>
+                    <a href="http://localhost/railway/new_user_form.html">New User</a>
+                </li>
+                <li>
+                    <a href="http://localhost/railway/enquiry.php" >Enquiry and Ticket Booking </a>
+                </li>
+                <li>
+                    <a href="http://localhost/railway/user_login.htm">Ticket History OR Cancellation</a>
+                </li>
+                <li>
+                    <a href="http://localhost/railway/admin_login.php">Admin Login</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+<div class="align-center admin-login-form">
 <?php 
 echo "
 <form action=\"admin_login1.php\" method=\"post\">
-User ID: <input type=\"text\" name=\"uid\" required><br>
+User ID: <input type=\"text\" name=\"uid\" required><br><br>
 Password: <input type=\"password\" name=\"password\" required><br>
-<input type=\"submit\">
+<input type=\"submit\" class=\"btn\">
 </form>
 ";
 ?>
-<br><a href="http://localhost/railway/index.htm">Go to Home Page!!!</a>
 </div>
+<footer class="footer">
+    <p>All rights reserved</p>
+    <div class="social-media">
+        <a href="https://www.facebook.com/IRCTCofficial/" target="_blank" class="social-icon"><i class='bx bxl-facebook'></i></a>
+        <a href="https://instagram.com/irctc.official" target="_blank" class="social-icon"><i class='bx bxl-instagram' ></i></a>
+        <a href="https://twitter.com/IRCTCofficial" target="_blank" class="social-icon"><i class='bx bxl-twitter' ></i></a>
+    </div>
+
+</footer>
 </body>
 </html>
